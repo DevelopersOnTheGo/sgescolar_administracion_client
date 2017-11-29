134fbfd690c2ed6811c889b481a5f3f622da8dd2
rogernina <rogerticonanina@gmail.com>
2017-11-29 01:52:55 -0500

    te

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -21,7 +21,9 @@ module.exports = {
       .waitForElementVisible('body', 5000)
       .assert.containsText('h4', 'Crear Docentes')
       .assert.elementCount('div', 67);
-        },
+      
+
+  },
 
 
   after : function(browser) {
